1066779ac84a67763e44727beb456287cc0f974a
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2024-05-12 18:29:47 +0300

    add

--- src/chekList/ChekList.jsx
+++ src/chekList/ChekList.jsx
@@ -1,37 +1,35 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import CygreBlack from "../assets/fonts/Cygre-Black.ttf";
-import CygreExtraBold from "../assets/fonts/Cygre-ExtraBoldIt.ttf";
-import Gilroy from "../assets/fonts/Gilroy-Medium.ttf";
 import "./ChekList.scss";
 import { privichki } from "./privichki";
 import Header from "../header/header";
+import { useCookies } from 'react-cookie';
 
 const ChekListHabits = styled.div`
-  color: #72ab46;
-  font-family: ${CygreBlack};
+  color: #4A860B;
+  font-family: 'CygreBlack';
   font-size: 54.723px;
   font-style: normal;
-  font-weight: 900;
+  font-weight: normal;
   line-height: normal;
 `;
 const ProgressOfHabits = styled.div`
   color: #b4d21c;
-  font-family: ${CygreExtraBold};
+  font-family: "CygreExtraBold";
   font-size: 30px;
   font-style: italic;
   font-weight: 800;
   line-height: normal;
-  margin-right: 50%;
-  width: 50%;
   margin-top: 2em;
+  text-align: left;
+  margin-bottom: 0.5em;
 `;
 
 const ChekBoxText = styled.a`
   margin-left: 1em;
   color: #696969;
   text-align: justify;
-  font-family: ${Gilroy};
+  font-family: "Gilroy";
   font-size: 20px;
   font-style: normal;
   font-weight: 500;
@@ -42,15 +40,38 @@ const ChekBoxDiv = styled.div`
   padding: 0.5em;
   margin-right: auto;
 `;
-let scoreHuman = 0;
-function functionReturnRadioButton(text, id) {
+
+const BoxHow = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30em;
+`;
+const ShowDiv = styled.div`
+  text-align: center;
+`
+const FlexDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: 5em;
+`
+const Slider = styled.input`
+    accent-color: #4A860B;
+    color: #CFCFCF;
+    width: 80%;   
+    border-radius: 0;
+    
+`;
+export default function ChekList() {
+  const [count, setCount] = useState(0);
+
+  function functionReturnRadioButton(text, id) {
   const Handle = () => {
     console.log(event.target.checked, event.target.value);
-    console.log(scoreHuman);
+    console.log(count);
     if (event.target.checked == true) {
-      scoreHuman += 1;
+      setCount(count + 1);
     } else {
-      scoreHuman -= 1;
+      setCount(count - 1);
     }
   };
   return (
@@ -70,21 +91,13 @@ function functionReturnRadioButton(text, id) {
 const ViewPrivichki = privichki.map((value) => {
   return functionReturnRadioButton(value.text, value.id);
 });
-const BoxHow = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-export default function ChekList() {
   return (
     <div>
       <Header></Header>
-      <div>
+      <ShowDiv>
         <ChekListHabits>ЧЕК-ЛИСТ ПРИВЫЧЕК</ChekListHabits>
-
-        <div>
+        <Slider value={count} max={20} min={0} type='range'></Slider>
+        <FlexDiv>
           <BoxHow>
             <ProgressOfHabits>Прогресс привычек</ProgressOfHabits>
             {ViewPrivichki}
@@ -101,8 +114,8 @@ export default function ChekList() {
             <ProgressOfHabits>Прогресс привычек</ProgressOfHabits>
             {ViewPrivichki}
           </BoxHow>
-        </div>
-      </div>
+        </FlexDiv>
+      </ShowDiv>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import "./index.css";
 import { CookiesProvider } from "react-cookie";
 import ChekList from "./chekList/ChekList.jsx";
 import App from "./App.jsx";
+import GlobalFont from './chekList/styleds.js';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CookiesProvider defaultSetOptions={{ path: "/" }}>
+    <GlobalFont/>
     <Test />
   </CookiesProvider>,
 );
